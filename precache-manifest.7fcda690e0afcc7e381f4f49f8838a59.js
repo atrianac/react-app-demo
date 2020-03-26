f35dfbd55edccf3538c0a50108deeb381d571e55
Alejandro Triana <Alejandro.Triana@endava.com>
2020-03-25 23:37:07 -0500

    Updates

---
+++ precache-manifest.7fcda690e0afcc7e381f4f49f8838a59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fac8fa2b872b5e44d3f8fbabce3eb0ba",
+    "url": "/react-app-demo/index.html"
+  },
+  {
+    "revision": "fa95278a359b5aff7f8d",
+    "url": "/react-app-demo/static/css/main.3470f4a8.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/react-app-demo/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app-demo/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa95278a359b5aff7f8d",
+    "url": "/react-app-demo/static/js/main.93758df0.chunk.js"
+  },
+  {
+    "revision": "6f680bfb62e01d5fafb0",
+    "url": "/react-app-demo/static/js/runtime-main.a29669e4.js"
+  }
+]);
